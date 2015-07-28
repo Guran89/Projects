971dd7facbcc76c2525c3beb211ecaf0ed4e88cd
Guran89 <kristoffer89.karlsson@gmail.com>
2015-07-28 13:46:09 +0200

    Updated script, with barplot instead of histogram

--- R/AuthorCount/author_count.R
+++ R/AuthorCount/author_count.R
@@ -7,14 +7,14 @@ data <- read.csv("data.csv", header = FALSE)
 #Set column name
 colnames(data) <- "author_count"
 
-#Subset top 30 most common numbers of collaborations
-data <- subset(data, author_count < 30)
+#Subset top 20 most common numbers of collaborations
+data <- subset(data, author_count < 21)
+data <- subset(data, author_count > 0) #Make sure there are no publications wihtout author
 
-#Create histogram
-hist(data$author_count, breaks = 25, col = "red", main = "Antal författare per publikation 2014",
+#Create barplot
+barplot(table(data$author_count), col = "red", main = "Antal författare per publikation 2014",
      xlab = "Antal författare", ylab = "Antal publikationer", las = 1, axes = FALSE)
 
-#Add axes and grid to histogram
-axis(1, seq(from = 0, to = 30, by = 2))
-axis(2, seq(from = 0, to = 3500, by = 500), las = 1)
+#Add axes and grid to barplot
+axis(2, seq(from = 0, to = 3500, by = 500), las = 1, tick = F)
 grid()
