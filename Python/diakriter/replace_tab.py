42322dfc244f453d0008c244f25d7b57cefae021
Guran89 <kristoffer89.karlsson@gmail.com>
2016-05-03 13:36:04 +0200

    New file for replacing tabs

---
+++ Python/diakriter/replace_tab.py
@@ -0,0 +1,34 @@
+#coding: utf-8
+
+
+cid = input("Please enter your CID:\nCID: ")
+in_data = input("The name of the file you want to convert:\nFilename (with extension): ")
+out_data = input("What do you want the new file to be named?\nFilename (without extension): ")
+data = open("/Users/"+cid+"/Desktop/"+in_data, "r").read()
+
+print("Counting occurrences of potential characters. Please wait.")
+
+pipe_count = 0
+hash_count = 0
+pound_count = 0
+dollar_count = 0
+for i in data:
+    if i == "|":
+        pipe_count = pipe_count + 1
+    elif i == "#":
+        hash_count = hash_count + 1
+    elif i == "£":
+        pound_count = pound_count + 1
+    elif i == "$":
+        dollar_count = dollar_count + 1
+
+print("Pipe (|): " + str(pipe_count))
+print("Hash (#): " + str(hash_count))
+print("Pound (£): " + str(pound_count))
+print("Dollar ($): " + str(dollar_count))
+
+new_char = input("What character would you like to replace with?\n>")
+data_replaced = data.replace("\t", new_char)
+newfile = open(out_data, "w")
+newfile.write(data_replaced)
+newfile.close()
--- Python/replace_newline.py
+++ Python/replace_newline.py
@@ -5,39 +5,44 @@ import os
 
 #Print welcome message and information what the script does.
 print ("***************************")
-print ("Script to replace whitelines.")
-print ("This script will replace every single whiteline with a a sign of your choice.")
+print ("Script to replace newlines.")
+print ("This script will replace every single newline with a a sign of your choice.")
+print ("Observe that the script only works with .txt files at the moment.")
 print ("***************************")
+
+#Script for computers running OSX
 if os.name == 'posix':
     cid = input("Please enter your CID:\nCID: ") #Let user enter CID
     old_ids = input("The name of the file you want to convert:\nFilename (without extension): ") #Let user choose which file to convert
     new_ids = input("What do you want the new file to be named?\nFilename (without extension): ") #Let user name the new file
-    replacement = input("What do you want to replace the newline with?\nReplace with: ")
-    data = open("/Users/"+cid+"/Desktop/"+old_ids+".txt", "r").read() #Open file with IDs
-    data_no_line = data.replace("\n", replacement) #Replace newlines with commas
+    replacement = input("What do you want to replace the newline with?\nReplace with: ") #Let user choose a sign that will replace the newline
+    data = open("/Users/"+cid+"/Desktop/"+old_ids+".txt", "r").read() #Open file to be modified
+    data_no_line = data.replace("\n", replacement) #Replace newlines with the chosen sign
     newfile = open("/Users/"+cid+"/Desktop/"+new_ids+".txt", "w") #Create new file called what the user choose .txt
-    newfile.write(data_no_line) #Write IDs to the new file
+    newfile.write(data_no_line) #Write to the new file
     newfile.close() #Close the file
-    print ("Ta-dah! File created.") #Message that the convertion is correct
+    print ("Ta-dah! File created.\nYou can find at: /Users/"+cid+"/Desktop/") #Message that the convertion is correct
 
     #Ask if the user wants the results printed out in the terminal
     choice = input("Do you want to open the new file?\n(Y/N) ")
     if choice == "Y" or choice == "y":
         print(open("/Users/"+cid+"/Desktop/"+new_ids+".txt", "r").read())
 
+#Script for computers running Windows
 if os.name == 'nt':
     cid = input("Please enter your CID:\nCID: ") #Let user enter CID
     old_ids = input("The name of the file you want to convert:\nFilename (without extension): ") #Let user choose which file to convert
     new_ids = input("What do you want the new file to be named?\nFilename (without extension): ") #Let user name the new file
-    replacement = input("What do you want to replace the newline with?\nReplace with: ")
-    data = open("D:\\Users\\"+cid+".NET\\Desktop\\"+old_ids+".txt", "r").read()
-    data_no_line = data.replace("\n", replacement) #Replace newlines with commas
-    newfile = open("\\Users\\"+cid+"\\Desktop\\"+new_ids+".txt", "w") #Create new file called what the user choose .txt
-    newfile.write(data_no_line) #Write IDs to the new file
+    replacement = input("What do you want to replace the newline with?\nReplace with: ") #Let user choose a sign that will replace the newline
+    harddrive = input("What harddrive do you use?\nHarddrive (D or C, for example):") #Which harddrive is the desktop located in?
+    data = open(harddrive+":\\Users\\"+cid+".NET\\Desktop\\"+old_ids+".txt", "r").read() #Open file to be modified
+    data_no_line = data.replace("\n", replacement) #Replace newlines with the chosen sign
+    newfile = open(harddrive+":\\Users\\"+cid+".NET\\Desktop\\"+new_ids+".txt", "w") #Create new file called what the user choose .txt
+    newfile.write(data_no_line) #Write to the new file
     newfile.close() #Close the file
-    print ("Ta-dah! File created.") #Message that the convertion is correct
+    print ("Ta-dah! File created.\nYou can find at: "+harddrive+":\\Users\\"+cid+".NET\\Desktop") #Message that the convertion is correct
 
     #Ask if the user wants the results printed out in the terminal
     choice = input("Do you want to open the new file?\n(Y/N) ")
     if choice == "Y" or choice == "y":
-        print(open("D:\\Users\\"+cid+".NET\\Desktop\\"+new_ids+".txt", "r").read())
+        print(open(harddrive+":\\Users\\"+cid+".NET\\Desktop\\"+new_ids+".txt", "r").read())
