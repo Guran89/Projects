ab1eaf1bb7411f9749e12cde0851c67bfadcba3c
Guran89 <kristoffer89.karlsson@gmail.com>
2016-07-01 13:00:08 +0200

    Add dept info in script, rather than in separate file

--- Python/split_institutions/publication_list.py
+++ Python/split_institutions/publication_list.py
@@ -1,10 +1,39 @@
+#encoding: utf-8
 import os, time
 
 if not os.path.exists("docs"):
     os.makedirs("docs")
 
+#IDs and names for departments
+ids = ["40;Institutionen för radio- och rymdvetenskap",
+"41;Institutionen för signaler och system",
+"46;Institutionen för tillämpad mekanik",
+"52;Institutionen för produkt- och produktionsutveckling",
+"55;Institutionen för teknisk fysik",
+"182;Institutionen för mikroteknologi och nanovetenskap",
+"366;Institutionen för data- och informationsteknik (Chalmers)",
+"367;Institutionen för arkitektur",
+"368;Institutionen för energi och miljö",
+"369;Institutionen för kemi- och bioteknik",
+"370;Institutionen för material- och tillverkningsteknik",
+"371;Institutionen för matematiska vetenskaper",
+"373;Institutionen för bygg- och miljöteknik",
+"374;Institutionen för sjöfart och marin teknik",
+"375;Institutionen för teknikens ekonomi och organisation",
+"523;Institutionen för fundamental fysik",
+"1643;Institutionen för tillämpad informationsteknologi (Chalmers)",
+"1695;Institutionen för rymd- och geovetenskap"]
+
+#Create empty list for splitted departments
+ids_splitted = []
+
+#Split departments and IDs and save in the list
+for i in ids:
+    splitted = i.split(";")
+    ids_splitted.append(splitted)
+
 print("\n*****************************************************")
-print("Make sure the department's ID is in the first column,\nand that all the IDs are in the ids.txt file.")
+print("Make sure the department's ID is in the first column in all documents.")
 print("*****************************************************\n")
 
 #Get the names of the files to include
@@ -22,7 +51,6 @@ overview_depts = []
 for row in data_overview:
     row_split = row.split(delimiter)
     overview_depts.append(row_split)
-
 
 data_per_year = open(per_year + ".txt", "r").read().replace(".", ",").split("\n")
 per_year_depts = []
@@ -42,15 +70,8 @@ for row in data_publist:
     row_split = row.split(delimiter)
     publist_depts.append(row_split)
 """
-#Load IDs, split them, and store them in a list
-ids_open = open("ids.txt", "r").read().split("\n")
-ids = [] #List to store the IDs
-#Split the IDs and store in the list
-for i in ids_open:
-    temp = i.split(";")
-    ids.append(temp)
 
-for num in ids:
+for num in ids_splitted:
     newfile = open("docs/" + str(num[0]) + "_" + num[1] + ".txt", "w")
 #Overview
     newfile.write("Number of journals|Number of publications|jf|jf x number of publications\n")
@@ -83,3 +104,5 @@ for num in ids:
             newfile.write("\n")
     newfile.close()
 """
+
+print("Success! The files can be found in " + os.getcwd() + "/docs")
