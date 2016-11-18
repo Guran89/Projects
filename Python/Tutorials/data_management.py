1d043dcfcb8bbeaaa0f46d8518f1ed4a5b213886
Guran89 <kristoffer89.karlsson@gmail.com>
2016-11-18 13:28:42 +0100

    Remove misplaced dirs

--- Python/Tutorials/data_management.py
+++
@@ -1,5 +0,0 @@
-#ID, Name, Description, OwnerID, Price, Condition, DateRegistered
-items = [
-["1", "Lawnmower", "Tool", "1", "$150", "Excellent", "2012-01-05"],
-["2", "Lawnmover", "Tool", "2", "$370", "Fair", "2012-04-01"],
-["3", "Bike", "3", "$200", "Fair", "2012-03-22"],
