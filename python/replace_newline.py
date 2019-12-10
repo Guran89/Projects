063612d6c2c725170b2718393e091406a350ffc5
Kristoffer Warnberg <kristoffer.warnberg@hb.se>
2019-12-10 10:38:15 +0100

    Adapt script to better work on windows

--- python/replace_newline.py
+++ python/replace_newline.py
@@ -5,14 +5,6 @@ All newlines will be replaced with a sign you choose"""
 import os
 import time
 
-#Print welcome message and information what the script does.
-print ("\n**************************************************************************")
-print ("Script to replace newlines.")
-print ("This script will replace every newline with a a sign of your choice.")
-print ("Observe that the script only works with .txt files at the moment.")
-print ("Instructions: Put the .txt file you want to convert on the desktop.\nFollow the steps below.")
-print ("**************************************************************************\n")
-
 #Create empty variables
 sign = ""
 old_ids = ""
@@ -20,7 +12,7 @@ new_ids = ""
 replacement = ""
 
 #Define function for user to enter information
-def enter_info():
+def enter_info_mac():
     global sign
     global old_ids
     global new_ids
@@ -30,9 +22,24 @@ def enter_info():
     new_ids = input("\nWhat do you want the new file to be named?\nFilename (without extension): ") #Let user name the new file
     replacement = input("\nWhat do you want to replace the newline with?\nReplace with: ") #Let user choose a sign that will replace the newline
 
+def enter_info_pc():
+    global old_ids
+    global new_ids
+    global replacement
+    old_ids = input("\nThe name of the file you want to convert:\nFilename (without extension): ") #Let user choose which file to convert
+    new_ids = input("\nWhat do you want the new file to be named?\nFilename (without extension): ") #Let user name the new file
+    replacement = input("\nWhat do you want to replace the newline with?\nReplace with: ") #Let user choose a sign that will replace the newline
+
 #Script for computers running OSX
 if os.name == 'posix':
-    enter_info()
+    #Print welcome message and information what the script does.
+    print ("\n**************************************************************************")
+    print ("Script to replace newlines.")
+    print ("This script will replace every newline with a a sign of your choice.")
+    print ("Observe that the script only works with .txt files at the moment.")
+    print ("Instructions: Put the .txt file you want to convert on the desktop.\nFollow the steps below.")
+    print ("**************************************************************************\n")
+    enter_info_mac()
     data = open("/Users/"+sign+"/Desktop/"+old_ids+".txt", "r").read() #Open file to be modified
     data_no_line = data.replace("\n", replacement) #Replace newlines with the chosen sign
     newfile = open("/Users/"+sign+"/Desktop/"+new_ids+".txt", "w") #Create new file called what the user choose .txt
@@ -49,18 +56,24 @@ if os.name == 'posix':
 
 #Script for computers running Windows
 if os.name == 'nt':
-    enter_info()
-    harddrive = input("\nWhat harddrive do you use?\nHarddrive (D or C, for example):") #Which harddrive is the desktop located in?
-    data = open(harddrive+":\\Users\\"+sign+"\\Desktop\\"+old_ids+".txt", "r").read() #Open file to be modified
+    #Print welcome message and information what the script does.
+    print ("\n**************************************************************************")
+    print ("Script to replace newlines.")
+    print ("This script will replace every newline with a a sign of your choice.")
+    print ("Observe that the script only works with .txt files at the moment.")
+    print ("Instructions: Put the .txt file you want to convert in the same directory as this script.\nFollow the steps below.")
+    print ("**************************************************************************\n")
+    enter_info_pc()
+    data = open(old_ids+".txt", "r").read() #Open file to be modified
     data_no_line = data.replace("\n", replacement) #Replace newlines with the chosen sign
-    newfile = open(harddrive+":\\Users\\"+sign+"\\Desktop\\"+new_ids+".txt", "w") #Create new file called what the user choose .txt
+    newfile = open(new_ids+".txt", "w") #Create new file called what the user choose .txt
     newfile.write(data_no_line) #Write to the new file
     newfile.close() #Close the file
     print("Working...")
     time.sleep(1) #Dramatic pause
-    print ("\nTa-dah! File created.\nYou can find it at: "+harddrive+":\\Users\\"+sign+"\\Desktop"+new_ids+".txt") #Message that the convertion is correct
+    print ("\nTa-dah! File created.\nYou can find it at: "+new_ids+".txt") #Message that the convertion is correct
 
     #Ask if the user wants the results printed out in the terminal
     choice = input("\nDo you want to see the results?\n(Y/N) ")
     if choice == "Y" or choice == "y":
-        print(open(harddrive+":\\Users\\"+sign+"\\Desktop\\"+new_ids+".txt", "r").read())
+        print(open(new_ids+".txt", "r").read())
